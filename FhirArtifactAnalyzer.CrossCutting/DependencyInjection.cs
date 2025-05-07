283bed1c975747df4c8da4316b98da45f726e175
Lucas Iglesias <lucasiglesias2704@gmail.com>
2025-05-07 00:27:39 -0300

    refactor and remove: removendo injeções não mais necessárias e dividindo os métodos de extensão em métodos menores, mais legíveis e que seguem melhor o 'S' do SOLID (Single Responsibility Principle)

--- FhirArtifactAnalyzer.CrossCutting/DependencyInjection.cs
+++ FhirArtifactAnalyzer.CrossCutting/DependencyInjection.cs
@@ -1,33 +1,45 @@
-﻿using FhirArtifactAnalyzer.Application.Parsers;
-using FhirArtifactAnalyzer.Application.Services;
+﻿using FhirArtifactAnalyzer.Application.Services;
 using FhirArtifactAnalyzer.Domain.Abstractions;
 using FhirArtifactAnalyzer.Domain.Settings;
+using FhirArtifactAnalyzer.Domain.Utils;
 using FhirArtifactAnalyzer.Domain.Validation;
 using FhirArtifactAnalyzer.Infrastructure;
-using FhirArtifactAnalyzer.Infrastructure.Utils;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace FhirArtifactAnalyzer.CrossCutting
 {
     public static class DependencyInjection
     {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IFhirParserFactory, FhirParserFactory>();
+
+            return services;
+        }
+
         public static IServiceCollection AddInfrastructure(this IServiceCollection services)
         {
-            services.AddScoped((provider) => DocumentStoreHolder.Store.OpenSession());
-            
-            var solutionDir = SolutionPathHelper.GetSolutionDirectory();
-            var workingDir = Path.Combine(Path.Combine(solutionDir, "FhirArtifactAnalyzer.ValidationArtifacts"));
+            services.AddScoped(_ => DocumentStoreHolder.Store.OpenSession());
+
+            return services;
+        }
+
+        public static IServiceCollection AddDomainServices(this IServiceCollection services)
+        {
+            services.AddScoped<IFhirValidator, LocalFhirValidator>();
+
+            return services;
+        }
+
+        public static IServiceCollection ConfigureGlobalOptions(this IServiceCollection services)
+        {
+            var solutionDir = SolutionPathFinder.GetSolutionDirectory();
+            var workingDir = Path.Combine(solutionDir, "FhirArtifactAnalyzer.ValidationArtifacts");
 
             services.Configure<ArtifactValidationSettings>(opts =>
             {
                 opts.WorkingDirectory = workingDir;
             });
-
-            services.AddScoped<JsonFhirParser>();
-            services.AddScoped<XmlFhirParser>();
-
-            services.AddScoped<IFhirParserFactory, FhirParserFactory>();
-            services.AddScoped<IFhirValidatorService, LocalFhirValidatorService>();
 
             return services;
         }

c2055c93ebdabe25a8cf233a0743d2fa9a5a3dfd
Lucas Iglesias <lucasiglesias2704@gmail.com>
2025-05-07 00:25:36 -0300

    refactor: adequando classe de factory do parser fhir para utilização da classe de constantes de extensão de arquivos, além da substituição das injeções de dependência via construtor para instanciação direta do objeto, para que a adaptação feita do design pattern Simple Factory seja devidamente respeitada

--- FhirArtifactAnalyzer.Application/Services/FhirParserFactory.cs
+++ FhirArtifactAnalyzer.Application/Services/FhirParserFactory.cs
@@ -1,28 +1,20 @@
 using FhirArtifactAnalyzer.Application.Parsers;
 using FhirArtifactAnalyzer.Domain.Abstractions;
+using FhirArtifactAnalyzer.Domain.Constants;
 
 namespace FhirArtifactAnalyzer.Application.Services
 {
     public class FhirParserFactory : IFhirParserFactory
     {
-        private readonly IFhirParser _jsonParser;
-        private readonly IFhirParser _xmlParser;
-
-        public FhirParserFactory(JsonFhirParser jsonParser, XmlFhirParser xmlParser)
-        {
-            _jsonParser = jsonParser;
-            _xmlParser = xmlParser;
-        }
-
         public IFhirParser GetParserForFile(string filename)
         {
             var extension = Path.GetExtension(filename).ToLowerInvariant();
 
             return extension switch
             {
-                ".json" => _jsonParser,
-                ".xml" => _xmlParser,
-                _ =>throw new NotSupportedException($"Don't support extension: {extension}")
+                FileExtensions.Json => new JsonParserStrategy(),
+                FileExtensions.Xml => new XmlParserStrategy(),
+                _ => throw new NotSupportedException($"Extension '{extension}' is not supported!")
             };
         }
     }

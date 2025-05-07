b86eeac49f24e17dcaa06fc70151268eb747d5ca
Lucas Iglesias <lucasiglesias2704@gmail.com>
2025-05-06 22:05:34 -0300

    chore; finish testing resource parsing

--- FhirArtifactAnalyzer.Cli/Program.cs
+++ FhirArtifactAnalyzer.Cli/Program.cs
@@ -1,6 +1,11 @@
 ﻿using FhirArtifactAnalyzer.CrossCutting;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Rest;
+using Hl7.Fhir.Serialization;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
 
 class Program
 {
@@ -8,6 +13,17 @@ class Program
     {
         using var host = CreateHost(args);
         using var scope = CreateScope(host);
+
+        var path = "C:\\Users\\Iglesias\\source\\repos\\FhirArtifactAnalyzer\\FhirArtifactAnalyzer.Cli\\OperationDefinition.json";
+        var conteudoJson = File.ReadAllText(path);
+        var parser = new FhirJsonParser();
+        var instance1 = parser.Parse<ValueSet>(conteudoJson).Name;
+        var instance2 = parser.Parse<ValueSet>(conteudoJson).Url;
+        var instance4 = parser.Parse<ValueSet>(conteudoJson).Description;
+
+        var instance5 = parser.Parse<ValueSet>(conteudoJson).TypeName;
+
+        var instance = parser.Parse<ValueSet>(conteudoJson);
         
         Console.WriteLine("Hello Fhir!");
     }

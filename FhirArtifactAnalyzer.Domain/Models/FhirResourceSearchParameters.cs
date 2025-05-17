1744f08ce1136914639844037262d665c0eb755e
Lucas Iglesias <lucasiglesias2704@gmail.com>
2025-05-17 14:48:03 -0300

    feat: added search parameters class for searching logic

---
+++ FhirArtifactAnalyzer.Domain/Models/FhirResourceSearchParameters.cs
@@ -0,0 +1,12 @@
+﻿namespace FhirArtifactAnalyzer.Domain.Models
+{
+    public class FhirResourceSearchParameters
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public string? TypeName { get; set; }
+        public string? Description { get; set; }
+        public string? Url { get; set; }
+        public string? Comment { get; set; }
+    }
+}

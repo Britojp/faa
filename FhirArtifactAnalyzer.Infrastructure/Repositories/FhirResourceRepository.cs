a4cd656ade8ebcbac53f3e6c6d61321dae74fe32
Lucas Iglesias <lucasiglesias2704@gmail.com>
2025-05-17 14:50:03 -0300

    feat: added repository for fhir resource to handle search logic chore: finish search logic

---
+++ FhirArtifactAnalyzer.Infrastructure/Repositories/FhirResourceRepository.cs
@@ -0,0 +1,46 @@
+﻿using FhirArtifactAnalyzer.Domain.Models;
+using FhirArtifactAnalyzer.Infrastructure.Indexes;
+using FhirArtifactAnalyzer.Infrastructure.Repositories.Abstractions;
+using Raven.Client.Documents.Session;
+
+namespace FhirArtifactAnalyzer.Infrastructure.Repositories
+{
+    public class FhirResourceRepository : Repository<FhirResource>
+    {
+        private readonly IDocumentSession _session;
+
+        public FhirResourceRepository(IDocumentSession session) : base(session)
+        {
+            _session = session;
+            CreateIndexes();
+        }
+
+        public IEnumerable<FhirResource> Search(FhirResourceSearchParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<FhirResource> Search(string searchTerm)
+        {
+            var query = _session
+                .Advanced
+                .DocumentQuery<FhirResource, FhirResource_BySearchingProperties>()
+                .Search(x => x.Name, searchTerm)
+                .OrElse()
+                .Search(x => x.TypeName, searchTerm)
+                .OrElse()
+                .Search(x => x.Description, searchTerm)
+                .OrElse()
+                .Search(x => x.Url, searchTerm)
+                .OrElse()
+                .Search(x => x.Comment, searchTerm);
+
+            return query.ToList();
+        }
+
+        private void CreateIndexes()
+        {
+            new FhirResource_BySearchingProperties().Execute(_session.Advanced.DocumentStore);
+        }
+    }
+}

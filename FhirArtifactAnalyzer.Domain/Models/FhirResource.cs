151f1ef5e9b80b8e91c76def61eb6ad7bcafd85d
Lucas Iglesias <lucasiglesias2704@gmail.com>
2025-05-17 14:45:32 -0300

    feat: added FhirResource class to represent the entiy thats going to represent the fhir resource saved in the database

---
+++ FhirArtifactAnalyzer.Domain/Models/FhirResource.cs
@@ -0,0 +1,16 @@
+﻿namespace FhirArtifactAnalyzer.Domain.Models
+{
+    public class FhirResource
+    {
+        public required string Id { get; init; }
+        public required string Name { get; set; }
+        public required string TypeName { get; set; }
+        public required string Description { get; set; }
+        public required string Url { get; set; }
+        public string? Comment { get; set; }
+
+        public required AttachmentInfo Attachment { get; init; }
+
+        public record AttachmentInfo(string Name, string ContentType);
+    }
+}

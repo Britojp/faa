358563f4cbc445dc05ae0800af0419050cd9f61e
Lucas Iglesias <lucasiglesias2704@gmail.com>
2025-05-19 23:28:47 -0300

    refactor: turned fhir search parameters class into a record having in mind its immutable behaviour

--- FhirArtifactAnalyzer.Domain/Models/FhirResourceSearchParameters.cs
+++ FhirArtifactAnalyzer.Domain/Models/FhirResourceSearchParameters.cs
@@ -1,12 +1,10 @@
 ﻿namespace FhirArtifactAnalyzer.Domain.Models
 {
-    public class FhirResourceSearchParameters
-    {
-        public string? Id { get; set; }
-        public string? Name { get; set; }
-        public string? TypeName { get; set; }
-        public string? Description { get; set; }
-        public string? Url { get; set; }
-        public string? Comment { get; set; }
-    }
+    public record FhirResourceSearchParameters(
+        string? Id,
+        string? Name,
+        string? TypeName,
+        string? Description,
+        string? Url,
+        string? Comment);
 }

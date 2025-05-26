f9f0bb3ec16992d2d27bf58b0c694fa35cde8418
Lucas Iglesias <lucasiglesias2704@gmail.com>
2025-05-26 20:05:25 -0300

    Revert "Merge branch 'master' of github.com:Britojp/faa into feature/implementando-modulo-indexacao-artefatos"
    This reverts commit 90ae8b090feb704fe6bea7c2136ebced3d8d0f41, reversing
    changes made to aeeda859204a01da54397bbc11e06a213ea79e52.

--- FhirArtifactAnalyzer.Blazor/Program.cs
+++ FhirArtifactAnalyzer.Blazor/Program.cs
@@ -1,37 +1,31 @@
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using FhirArtifactAnalyzer;
-using FhirArtifactAnalyzer.Services;
-using System;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Blazored.LocalStorage;
+using FhirArtifactAnalyzer.Blazor.Components;
+using FhirArtifactAnalyzer.CrossCutting;
 
-namespace FhirArtifactAnalyzer
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructure();
+builder.Services.AddDomainServices();
+builder.Services.ConfigureGlobalOptions();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
-            builder.RootComponents.Add<HeadOutlet>("head::after");
-
-            // Register HTTP client for API communication
-            builder.Services.AddScoped(sp => new HttpClient
-            {
-                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
-            });
-
-            // Register custom services
-            builder.Services.AddScoped<FhirApiService>();
-            builder.Services.AddScoped<GraphService>();
-            builder.Services.AddScoped<ExportService>();
-
-            // Configure local storage if needed
-            builder.Services.AddBlazoredLocalStorage();
-
-            await builder.Build().RunAsync();
-        }
-    }
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();

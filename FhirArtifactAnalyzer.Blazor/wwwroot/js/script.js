e4fd97932b60e16906f36a763c775fc4b3a4d71a
PC <PC@DESKTOP-0K6G4LL>
2025-06-13 20:56:06 -0300

    fix: ajuste no funcionamento no upload de arquivos

---
+++ FhirArtifactAnalyzer.Blazor/wwwroot/js/script.js
@@ -0,0 +1,13 @@
+﻿    window.blazorFileDrop = {
+        preventDragDefaults: function (elementId) {
+            const el = document.getElementById(elementId);
+    if (!el) return;
+
+            ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+        el.addEventListener(eventName, (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+        }, false);
+            });
+        }
+    };
